96c153576a8862644ed89763d8b5a8b480651acd
Daria <d.zherebcova@medpoint24.ru>
2021-04-21 22:34:05 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// подключение express
+const express = require("express");
+// создаем объект приложения
+const app = express();
+app.set("view engine", "hbs");
+// определяем обработчик для маршрута "/"
+app.use("/", function(request, response){
+     
+  response.render("contact.hbs", {
+    width: "300px",
+    height: "200px",
+  });
+});
+// начинаем прослушивать подключения на 3000 порту
+app.listen(3000);
