d14bca41445863ffed7dbfaf448f669052ab35d7
Daria Zherebtsova <d.zherebcova@medpoint24.ru>
2021-04-22 15:13:37 +0300

    add 2border-slash template

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ app.set("view engine", "hbs");
 // определяем обработчик для маршрута "/"
 app.use("/", function(request, response){
      
-  response.render("contact.hbs", {
-    width: "300px",
+  response.render("2borders-slash.hbs", {
+    width: "400px",
     height: "220px",
+    fio: "Иванов Иван Иванович",
+    type: "предрейсовый",
+    date: "2020-04-1 20:20:41",
+    medic: "Петрова Мария Ивановна",
+    medicEDS: "6gh585ire9e3k38ef67f"
   });
 });
 // начинаем прослушивать подключения на 3000 порту
