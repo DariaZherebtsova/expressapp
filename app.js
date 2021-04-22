6bae97853fa30b8e4aa92da9a9d76fb484bba07c
Daria Zherebtsova <d.zherebcova@medpoint24.ru>
2021-04-22 17:27:02 +0300

    change size

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ app.set("view engine", "hbs");
 // определяем обработчик для маршрута "/"
 app.use("/", function(request, response){
      
-  response.render("2borders.hbs", {
-    width: "400px",
-    height: "220px",
-    fio: "Иванов Иван Иванович",
-    type: "предрейсовый",
+  response.render("2borders-slash.hbs", {
+    width: "210px",
+    height: "110px",
+    fio: "12345678901 23456789012345 67890123456789",
+    reason: "Отклонение АД (156/97)",
+    repeat: "Пройти повторно через 20 мин!",
     date: "2020-04-1 20:20:41",
     medic: "Петрова Мария Ивановна",
     medicEDS: "6gh585ire9e3k38ef67f"
